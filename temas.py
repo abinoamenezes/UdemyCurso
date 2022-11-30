4de5426b670a4eb8e87dd72148fae6bdc3684515
Abinoã Menezes <abinoamenezes89@gmail.com>
2022-11-29 21:02:49 -0300

    Update temas.py

--- temas.py
+++ temas.py
@@ -3,11 +3,19 @@ from tkinter import ttk
 
 class RootMain (tk.Tk):
     def __init__(self):
-        super.__init__()
+        super().__init__()
+        self.geometry('500x250+200+150')
         self.nome=ttk.Label(self, text='Nome: ')
         self.nome.place(x=5,y= 10)
 
+        self.entry_nome= ttk.Entry(self)
+        self.entry_nome.place(x=60,y=10)
 
+        self.button=ttk.Button(self, text='Executar')
+        self.button.place(y=10,x=260)
+
+        self.tema=ttk.Label(self, text= 'Tema')
+        self.tema.place(x=5,y=45)
 
 root=RootMain()
 root.mainloop()
