956465c9679f537723082c56bdae97b8d3d96822
Abinoã Menezes <abinoamenezes89@gmail.com>
2023-02-19 20:51:20 -0300

    Update brinquis.py

--- brinquis.py
+++ brinquis.py
@@ -1,3 +1,12 @@
+import time
 
-nome= input('olá, digite teu nome: ')
-print('olá '  + nome)
+def semaforo():
+    while True:
+        print("Sinal vermelho")
+        time.sleep(5)
+        print("Sinal amarelo")
+        time.sleep(2)
+        print("Sinal verde")
+        time.sleep(5)
+
+semaforo()
