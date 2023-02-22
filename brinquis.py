607217d3759aaa4b9136cedda0f5f78bdc2dfa51
Abinoã Menezes <abinoamenezes89@gmail.com>
2023-02-22 13:01:27 -0300

    .

--- brinquis.py
+++ brinquis.py
@@ -1,12 +1,29 @@
 import time
 
-def semaforo():
-    while True:
-        print("Sinal vermelho")
-        time.sleep(5)
-        print("Sinal amarelo")
-        time.sleep(2)
-        print("Sinal verde")
-        time.sleep(5)
+# Define as cores dos semáforos
+VERMELHO = 'vermelho'
+AMARELO = 'amarelo'
+VERDE = 'verde'
 
-semaforo()
+# Define o tempo que cada cor ficará acesa (em segundos)
+TEMPO_VERMELHO = 5
+TEMPO_AMARELO = 2
+TEMPO_VERDE = 5
+
+# Função para acender uma cor e apagar as outras duas
+def acender_cor(cor):
+    if cor == VERMELHO:
+        print('Semáforo: vermelho')
+        time.sleep(TEMPO_VERMELHO)
+    elif cor == AMARELO:
+        print('Semáforo: amarelo')
+        time.sleep(TEMPO_AMARELO)
+    elif cor == VERDE:
+        print('Semáforo: verde')
+        time.sleep(TEMPO_VERDE)
+
+# Loop infinito para manter o semáforo funcionando
+while True:
+    acender_cor(VERMELHO)
+    acender_cor(AMARELO)
+    acender_cor(VERDE)
